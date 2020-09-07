cb2802a5060e511057a7540619798173319163e8
JEdeTitto <julianeloy.detitto@gmail.com>
2020-09-07 11:37:42 -0300

    Fix team

--- app/src/main/java/com/example/tptiendamobile/Mock.kt
+++ app/src/main/java/com/example/tptiendamobile/Mock.kt
@@ -9,10 +9,10 @@ fun mockTeam(): MutableList<Team> {
     teamList.add(Team("https://ci4.googleusercontent.com/proxy/0SygDKCTFCfgDbMQ4ggMW6pTG7mnR3GYU3VqwOkbM_pwEtdZPHfKBE-uvBSwh21C4r958Jf9aZ1UexZE3bIHUawrriH29Zb-dZlJQpAS=s0-d-e1-ft#https://avatars3.githubusercontent.com/u/64224981?s=120&v=4", "Javier Grassano", "Backend"))
     teamList.add(Team("https://ca.slack-edge.com/T636X490R-URAPKEKPT-6c48a543ecc4-512", "Julian de Titto", "Android"))
     teamList.add(Team("https://pps.whatsapp.net/v/t61.24694-24/57299491_435364077039637_2786225330220695552_n.jpg?oh=b0cc57e5904c8727206addc07d9f881c&oe=5F5AC588", "Hernan Cortes", "Front end"))
-    teamList.add(Team("https://pps.whatsapp.net/v/t61.24694-24/76814210_598766590701224_2219921241861158383_n.jpg?oh=c9959bc6307c27f395f4f1df51697b63&oe=5F5A0372", "Juan Beltramino", "Front end"))
+    teamList.add(Team("https://media-exp1.licdn.com/dms/image/C4E03AQHDkIYtN9z41g/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=yEmYM3Yak_6o4k5-JvcBJFo-1PPMJ7NTszgGZewUdeE", "Juan Beltramino", "Front end"))
     teamList.add(Team("https://pps.whatsapp.net/v/t61.24694-24/64382691_2443235555761207_2978949227280859136_n.jpg?oh=4bd50936e37dfd5a99f34a940247bee9&oe=5F5A4EB4", "Juan Gonzales", "Front end"))
-    teamList.add(Team("https://pps.whatsapp.net/v/t61.24694-24/97746972_174150997284758_2413528895520057072_n.jpg?oh=41455089e29dc26fa3b6d1302ffd5faf&oe=5F2C6B77", "Agustin Zalazar", "Front end"))
-    teamList.add(Team("https://pps.whatsapp.net/v/t61.24694-24/64382691_2443235555761207_2978949227280859136_n.jpg?oh=999c2a63962cea3d3b58445bec4ac680&oe=5F3AAAB4", "Bruno Mahiques", "Front end"))
+    teamList.add(Team("https://instagram.faep8-2.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/89965720_134255664777892_6617766245046632186_n.jpg?_nc_ht=instagram.faep8-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=qeJ-c8WqHA4AX9ZhL3C&oh=d324277b3e1f1dc33711c6b28f0ef116&oe=5F7F1C50", "Agustin Zalazar", "Front end"))
+    teamList.add(Team("https://media-exp1.licdn.com/dms/image/C4E03AQGirc6qtt2QsQ/profile-displayphoto-shrink_200_200/0?e=1605139200&v=beta&t=wiFAT-EJ4MK_LiyhMblhjrrR_Wa_AECLW3SCA9ISFqA", "Bruno Mahiques", "Front end"))
 
     return teamList
 }
